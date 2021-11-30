838b33aa1eb6be7ca8307d36692c29c6422cdbc5
leejyeone <78450616+leejyeone@users.noreply.github.com>
2021-11-30 18:51:26 +0900

    Update lab2-3.sh

--- src/lab2-3.sh
+++ src/lab2-3.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 w=$1
-h=$2
-BMI=`"$w/($h*$h)"|bc`
+h=$( echo "scale=2; ${2}.0/100" |bc )
+bmi=$( echo "scale=1; ${w}.0/(${h}*${h})" |bc )
 
-if [ $BMI < 18.5 ]
+if [ `echo "$bmi >= 18.5" | bc` -eq 1 ] && [ `echo "23.0 > $bmi" | bc` -eq 1 ]
 then
-	echo "You are underweight"
-elif [ $BMI < 20 ]
+        echo "정상체중입니다."
+elif [ `echo "$bmi < 18.5" | bc` -eq 1 ]
 then
-	echo "You are normalweight"
+        echo "저체중입니다."
 else
-	echo "You are overweight"
+        echo "과체중입니다."
 fi
 
 exit 0
-	
+
